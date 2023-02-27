73b24656bd8a3a73e8c52bf2a0ad75321da08455
gposadasDEV <202002068@estudiante.udv.edu.gt>
2023-02-27 02:07:32 -0600

    GetComponet
    LLamada a otro componente

--- Assets/Scripts/HelloWorld/ControlCuadrado.cs
+++ Assets/Scripts/HelloWorld/ControlCuadrado.cs
@@ -2,23 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class ControlCuadrado : MonoBehaviour
 {
-    [SerializeField] private string mensaje;
     // Start is called before the first frame update
+    HelloWorld hw;
     void Start()
     {
-        
+        hw = hw.GetComponent<HelloWorld>();
+        hw.Saludar();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    public void Saludar()
-    {
-        Debug.Log(mensaje);
     }
 }
